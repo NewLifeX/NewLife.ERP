16e78f59ab9cc97ab125242e9701f5cf1c0e1b0b
大石头 <nnhy@vip.qq.com>
2022-09-02 11:27:49 +0800

    移库页面操作

--- NewLife.ERP/Areas/Products/Controllers/ProductStockController.cs
+++ NewLife.ERP/Areas/Products/Controllers/ProductStockController.cs
@@ -1,6 +1,11 @@
-﻿using Erp.Data.Products;
+﻿using Erp.Data.Models;
+using Erp.Data.Products;
+using Microsoft.AspNetCore.Mvc;
 using NewLife.Cube;
+using NewLife.ERP.Areas.Products.Models;
+using NewLife.ERP.Services;
 using NewLife.Web;
+using XCode.Membership;
 
 namespace NewLife.ERP.Areas.Products.Controllers;
 
@@ -8,10 +13,30 @@ namespace NewLife.ERP.Areas.Products.Controllers;
 [Menu(0, false)]
 public class ProductStockController : EntityController<ProductStock>
 {
-    static ProductStockController() => LogOnChange = true;
+    private readonly StockService _stockService;
+
+    static ProductStockController()
+    {
+        LogOnChange = true;
+
+        {
+            var df = ListFields.AddListField("Move", null, "Quantity");
+            df.DisplayName = "移库";
+            df.Url = "ProductStock/Move?id={Id}";
+        }
+    }
+
+    public ProductStockController(StockService stockService) => _stockService = stockService;
 
     protected override IEnumerable<ProductStock> Search(Pager p)
     {
+        var id = p["id"].ToInt(-1);
+        if (id > 0)
+        {
+            var entity = ProductStock.FindById(id);
+            if (entity != null) return new[] { entity };
+        }
+
         var productId = p["productId"].ToInt(-1);
         //var unitName = p["unitName"];
         var warehouseId = p["warehouseId"].ToInt(-1);
@@ -20,5 +45,45 @@ public class ProductStockController : EntityController<ProductStock>
         var end = p["dtEnd"].ToDateTime();
 
         return ProductStock.Search(productId, warehouseId, start, end, p["Q"], p);
+    }
+
+    /// <summary>移库</summary>
+    /// <returns></returns>
+    [EntityAuthorize(PermissionFlags.Update)]
+    public ActionResult Move(Int32 id) => View("Move", new MoveStockModel { Id = id });
+
+    /// <summary>移库</summary>
+    /// <returns></returns>
+    [EntityAuthorize(PermissionFlags.Update)]
+    public ActionResult MoveSave(MoveStockModel model)
+    {
+        try
+        {
+            var ps = ProductStock.FindById(model.Id);
+
+            var stockModel = new StockModel
+            {
+                ProductId = ps.ProductId,
+                WarehouseId = ps.WarehouseId,
+                Quantity = ps.Quantity,
+            };
+
+            _stockService.Move(stockModel, model.WarehouseId);
+
+            return RedirectToAction("Index", new { productId = ps.ProductId });
+        }
+        catch (Exception ex)
+        {
+            if (ex is ArgumentException aex)
+                ModelState.AddModelError(aex.ParamName, aex.Message);
+            else
+                ModelState.AddModelError("", ex.Message);
+
+            ViewBag.StatusMessage = "移库失败！" + ex.Message;
+
+            WriteLog("Move", false, ex.Message);
+
+            return View("Move", model);
+        }
     }
 }
---
+++ NewLife.ERP/Areas/Products/Models/MoveStockModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NewLife.ERP.Areas.Products.Models;
+
+public class MoveStockModel
+{
+    public Int32 Id { get; set; }
+
+    public Int32 WarehouseId { get; set; }
+
+    public Int32 Quantity { get; set; }
+}
